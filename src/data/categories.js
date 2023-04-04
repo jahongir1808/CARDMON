04b8eecfd27f2eeee1d5009e14990ad9e11f6073
jahongir1808 <jahongir.developer18@gmail.com>
2023-04-04 21:51:01 +0500

    sdasd

---
+++ src/data/categories.js
@@ -0,0 +1,37 @@
+export const categories = [
+  {
+    id: "1",
+    name: "Авто",
+  },
+  {
+    id: "2",
+    name: "Ком.услуги",
+  },
+  {
+    id: "3",
+    name: "Налоги",
+  },
+  {
+    id: "4",
+    name: "Другое",
+  },
+];
+
+export const Debtscategories = [
+  {
+    id: "1",
+    name: "Аренда",
+  },
+  {
+    id: "2",
+    name: "Банк услуги",
+  },
+  {
+    id: "3",
+    name: "Покупка",
+  },
+  {
+    id: "4",
+    name: "Другое",
+  },
+];
